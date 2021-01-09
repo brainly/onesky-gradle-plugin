e2d693c2a791b743f4166c28fdbe4333ea3baad0
Marcin Robaczyński <scanarch@gmail.com>
2021-01-09 17:50:40 +0100

    Update gradle plugin, update kotlin to 1.4.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+        classpath("com.android.tools.build:gradle:3.5.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
     }
 }
